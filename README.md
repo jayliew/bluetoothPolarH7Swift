4109d7c58e95213ac0a988d17ec1ae8239b9d60a
Jay Liew <jaysern@gmail.com>
2017-02-05 11:07:18 -0800

    first

---
+++ README.md
@@ -0,0 +1,5 @@
+# Bluetooth Low Energy BTLE "hello world" iOS (Swift) with the Polar H7 Heart Rate Monitor
+
+Syntax conversion of gist from @nolili 's work here: https://gist.github.com/nolili/a583ea045dafafebb17f
+
+Compiles on Xcode 8.2
--- bluetoothPolarH7/ViewController.swift
+++ bluetoothPolarH7/ViewController.swift
@@ -1,25 +1,137 @@
-//
-//  ViewController.swift
-//  bluetoothPolarH7
-//
-//  Created by Jay Liew on 2/5/17.
-//  Copyright © 2017 Jay Liew. All rights reserved.
-//
-
 import UIKit
+import CoreBluetooth
 
-class ViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+class ViewController: UIViewController, CBCentralManagerDelegate, CBPeripheralDelegate {
+    
+    var centralManager:CBCentralManager!
+    var connectingPeripheral:CBPeripheral!
+    
+    required init?(coder aDecoder: NSCoder) {
+        
+        super.init(coder: aDecoder)
+        centralManager = CBCentralManager(delegate: self, queue: DispatchQueue.main)
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    func centralManagerDidUpdateState(_ central: CBCentralManager){
+        
+        switch central.state{
+        case .poweredOn:
+            print("poweredOn")
+            
+            let serviceUUIDs:[AnyObject] = [CBUUID(string: "180D")]
+            let lastPeripherals = centralManager.retrieveConnectedPeripherals(withServices: serviceUUIDs as! [CBUUID])
+            
+            if lastPeripherals.count > 0{
+                let device = lastPeripherals.last! as CBPeripheral;
+                connectingPeripheral = device;
+                centralManager.connect(connectingPeripheral, options: nil)
+            }
+            else {
+                centralManager.scanForPeripherals(withServices: serviceUUIDs as? [CBUUID], options: nil)
+                
+            }
+            
+        default:
+            print(central.state)
+        }
     }
-
-
+    
+    func centralManager(central: CBCentralManager!, didDiscoverPeripheral peripheral: CBPeripheral!, advertisementData: [NSObject : AnyObject]!, RSSI: NSNumber!) {
+        
+        connectingPeripheral = peripheral
+        connectingPeripheral.delegate = self
+        centralManager.connect(connectingPeripheral, options: nil)
+    }
+    
+    func centralManager(central: CBCentralManager!, didConnectPeripheral peripheral: CBPeripheral!) {
+        
+        peripheral.discoverServices(nil)
+    }
+    
+    func peripheral(peripheral: CBPeripheral!, didDiscoverServices error: NSError!) {
+        
+        if (error) != nil{
+            
+        }
+        else {
+            for service in peripheral.services as [CBService]!{
+                peripheral.discoverCharacteristics(nil, for: service)
+            }
+        }
+    }
+    
+    func peripheral(peripheral: CBPeripheral!, didDiscoverCharacteristicsForService service: CBService!, error: NSError!) {
+        
+        if (error) != nil{
+            
+        }
+        else {
+            
+            if service.uuid == CBUUID(string: "180D"){
+                for characteristic in service.characteristics! as [CBCharacteristic]{
+                    switch characteristic.uuid.uuidString{
+                        
+                    case "2A37":
+                        // Set notification on heart rate measurement
+                        print("Found a Heart Rate Measurement Characteristic")
+                        peripheral.setNotifyValue(true, for: characteristic)
+                        
+                    case "2A38":
+                        // Read body sensor location
+                        print("Found a Body Sensor Location Characteristic")
+                        peripheral.readValue(for: characteristic)
+                        
+                    case "2A39":
+                        // Write heart rate control point
+                        print("Found a Heart Rate Control Point Characteristic")
+                        
+                        var rawArray:[UInt8] = [0x01];
+                        let data = NSData(bytes: &rawArray, length: rawArray.count)
+                        peripheral.writeValue(data as Data, for: characteristic, type: CBCharacteristicWriteType.withoutResponse)
+                        
+                    default:
+                        print()
+                    }
+                    
+                }
+            }
+        }
+    }
+    
+    func update(heartRateData:Data){
+        
+        var buffer = [UInt8](repeating: 0x00, count: heartRateData.count)
+        heartRateData.copyBytes(to: &buffer, count: buffer.count)
+        
+        var bpm:UInt16?
+        if (buffer.count >= 2){
+            if (buffer[0] & 0x01 == 0){
+                bpm = UInt16(buffer[1]);
+            }else {
+                bpm = UInt16(buffer[1]) << 8
+                bpm =  bpm! | UInt16(buffer[2])
+            }
+        }
+        
+        if let actualBpm = bpm{
+            print(actualBpm)
+        }else {
+            print(bpm!)
+        }
+    }
+    
+    @nonobjc func peripheral(peripheral: CBPeripheral!, didUpdateValueForCharacteristic characteristic: CBCharacteristic!, error: NSError!) {
+        
+        if (error) != nil{
+            
+        }else {
+            switch characteristic.uuid.uuidString{
+            case "2A37":
+                update(heartRateData:characteristic.value!)
+                
+            default:
+                print()
+            }
+        }
+    }
 }
-
